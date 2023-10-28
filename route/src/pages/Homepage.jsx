23c89ab3c50250b4448d30782d025653ea6d0718
yersultan <erserstur@gmail.com>
2023-10-28 16:20:19 +0600

    111

---
+++ route/src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+const Homepage = () => {
+    return (
+        <div>
+            <h1 className="homeh1">Home</h1>
+            <h2>That's all for home page</h2>
+        </div>
+    )
+}
+
+export {Homepage}
